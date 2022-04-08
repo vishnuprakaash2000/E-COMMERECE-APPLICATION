1757bdf36fcdfbf59e4708e60c02c6c4f22df877
Vishnu Prakaash R <81284425+vishnuprakaash2000@users.noreply.github.com>
2022-04-08 10:18:01 +0530

    Update alterations.cs

--- OnlineShopping-master/OnlineShopping-master/Controllers/alterations.cs
+++ OnlineShopping-master/OnlineShopping-master/Controllers/alterations.cs
@@ -43,7 +43,40 @@ public ActionResult PartialViewExample()
 }                       
                      
 
-
+//Exceptional handling
+                       
+using System;
+class MyClass
+{
+    public void Method()
+    {
+        try
+        {
+            int x = 0;
+            int sum = 100 / x;
+        }
+        catch (DivideByZeroException)
+        {
+            throw;
+        }
+    }
+}
+class MyClient
+{
+    public static void Main()
+    {
+        MyClass mc = new MyClass();
+        try
+        {
+            mc.Method();
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("Exception caught here");
+        }
+        Console.WriteLine("LAST STATEMENT");
+    }
+}
 
 
 
